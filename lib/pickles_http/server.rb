57fedfe24d00d05f07c26c84ef075ca127c294a6
Moisés <daw.moisesguerola@gmail.com>
2024-02-12 13:33:16 +0100

    Added Threads & Mutext, error on cors middleware

--- lib/pickles_http/server.rb
+++ lib/pickles_http/server.rb
@@ -1,4 +1,5 @@
 require 'socket'
+require 'concurrent'
 require_relative 'utils'
 require_relative 'router'
 require_relative 'logger'
@@ -9,18 +10,17 @@ class PicklesHttpServer
 
     def initialize(port, log_file)
       @server = TCPServer.new(port)
-      @router = Router.new
       @port = port
+      @router = Router.new
       @middlewares = []
       @logger = PicklesHttpServer::Logger.new(log_file)
       @not_found_message = 'No route here'
+      @request_queue = Queue.new
+      @write_mutex = Mutex.new
     end
 
     def change_server_option(option, value)
-      case option
-      when 'set_not_found_message'
-        @not_found_message = value
-      end
+      @not_found_message = value if option == 'set_not_found_message'
     end
 
     def add_route(method, path, handler)
@@ -33,10 +33,22 @@ class PicklesHttpServer
 
     def start
       puts "PicklesServer is running on http://localhost:#{@port} 🔥"
+      start_request_processing_thread
+
       loop do
         client = @server.accept
-        handle_request(client)
-        client.close
+        @request_queue.push(client)
+      end
+    end
+
+    private
+
+    def start_request_processing_thread
+      Thread.new do
+        loop do
+          client = @request_queue.pop
+          handle_request(client)
+        end
       end
     end
 
@@ -45,19 +57,60 @@ class PicklesHttpServer
       return if request_line.nil?
 
       method, path = request_line.split
+      headers = read_headers(client)
+      body = read_body(client, headers['Content-Length'].to_i)
+
       handler = @router.route_request(method, path)
+      promises = []
 
-      @middlewares.each { |middleware| middleware.call(client, method, path, @logger) }
-
-      if handler
-        handler.call(client, path)
-      else
-        handle_unknown_request(client)
+      promises << Concurrent::Promises.future do
+        @write_mutex.synchronize do
+          Middlewares::catcher.call(client, method, path, @logger)
+        end
       end
+
+      @middlewares.each do |middleware|
+        promises << Concurrent::Promises.future do
+          @write_mutex.synchronize do
+            middleware.call(client, body, headers)
+          end
+        end
+      end
+
+      Concurrent::Promises.zip(*promises).then do
+        if handler
+          if !client.closed?
+            handler.call(client, body, headers) 
+          else
+            Response::send_response(client, "Socket Closed", HttpStatusCodes::ERROR)
+          end
+        else
+          handle_unknown_request(client)
+        end
+      end.value!
+    rescue StandardError => e
+      @logger.log("Error handling request: #{e.message}", LogMode::ERROR)
+    ensure
+      client.close unless client.closed?
     end
 
     def handle_unknown_request(client)
       Response::send_response(client, @not_found_message, HttpStatusCodes::BAD_REQUEST)
+    end
+
+    def read_headers(client)
+      headers = {}
+      loop do
+        line = client.gets.chomp
+        break if line.empty?
+        key, value = line.split(': ', 2)
+        headers[key] = value
+      end
+      headers
+    end
+
+    def read_body(client, content_length)
+      content_length > 0 ? client.read(content_length) : ''
     end
   end
 end
--- lib/pickles_http/utils.rb
+++ lib/pickles_http/utils.rb
@@ -6,21 +6,39 @@ module HttpStatusCodes
 end
 
 class Response
-  def self.send_response(client, body, status = HttpStatusCodes::OK)
+  def self.send_response(client, body, status = HttpStatusCodes::OK, content_type = ContentTypes::HTML, custom_headers: [])
     client.puts "HTTP/1.1 #{status}"
-    client.puts "Content-Type: #{ContentTypes::HTML}"
+    client.puts "Content-Type: #{content_type}"
+
+    custom_headers.each { |header| client.puts header }
+
     client.puts
-    client.puts body
+    client.puts body unless body.nil?
+    client.close unless client.closed?
   end
 end
 
 class Middlewares
   class << self
-    attr_accessor :catcher
+    attr_accessor :catcher, :cors
   end
 
   self.catcher = lambda do |_, method, path, logger|
     logger.log("[#{method}] - #{path}", LogMode::INFO)
+  end
+
+  self.cors = lambda do |client, _, _|
+    headers_cors = [
+      'Access-Control-Allow-Origin: *',
+      'Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS',
+      'Access-Control-Allow-Headers: Content-Type, Authorization',
+      'Access-Control-Max-Age: 86400'
+    ]
+
+    if !client.closed?
+      headers_cors.each { |header| client.puts header }
+      client.puts
+    end
   end
 end
 
@@ -29,6 +47,7 @@ module RequestMethods
   POST = 'POST'
   PUT = 'PUT'
   DELETE = 'DELETE'
+  OPTIONS = 'OPTIONS'
 end
 
 module ContentTypes
